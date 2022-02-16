529703fd7c2192091982adf3c0221bcf958cec2f
PG-Momik <momik12339@gmail.com>
2022-02-16 11:13:36 +0545

    Added CRUD to endpoints

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header('Access-Control-Allow-Method: POST, READ, PUT, DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 require_once 'Model/Database.php';
 require_once 'Model/User.php';
@@ -20,21 +16,87 @@ $route = new Route($db);
 $n_contribution = new NodeContribution($db);
 $r_contribution = new RouteContribution($db);
 
-$url_components = explode("/", $_SERVER['REQUEST_URI']);
-$method = $_SERVER['REQUEST_METHOD'];
-$op = $_GET['op'];
-$en = $_GET['en'];
 
 switch ($en) {
     case 'user':
+        switch($operation){
+            case "C":
+                break;
+            case "R1";
+                break;
+            case "R":
+                break;
+            case "U":
+                break;
+            case "D":
+                break;
+            default:
+                break;
+        }
         break;
     case "node":
+        switch($operation){
+            case "C":
+                break;
+            case "R1";
+                break;
+            case "R":
+                break;
+            case "U":
+                break;
+            case "D":
+                break;
+            default:
+                break;
+        }
         break;
     case 'routes':
+        switch($operation){
+            case "C":
+                break;
+            case "R1";
+                break;
+            case "R":
+                break;
+            case "U":
+                break;
+            case "D":
+                break;
+            default:
+                break;
+        }
         break;
     case 'contribute_node':
+        switch($operation){
+            case "C":
+                break;
+            case "R1";
+                break;
+            case "R":
+                break;
+            case "U":
+                break;
+            case "D":
+                break;
+            default:
+                break;
+        }
         break;
     case 'contribute_route':
+        switch($operation){
+            case "C":
+                break;
+            case "R1";
+                break;
+            case "R":
+                break;
+            case "U":
+                break;
+            case "D":
+                break;
+            default:
+                break;
+        }
             break;
     default:
         break;
