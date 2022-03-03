1ea479b313f69f9e0a3f077f5f004336803e29cf
PG-Momik <momik12339@gmail.com>
2022-03-04 00:01:02 +0545

    Add read_RouteToNode(). Works. Better. Use this for Form type 2.

--- Model/Route.php
+++ Model/Route.php
@@ -232,5 +232,80 @@ class Route
         echo json_encode($response);
     }
 
+    public function read_routeToNode($node)
+    {
+        //get rows where node is mentioned
+        $query = "SELECT r.id, r.path, r.route_no
+        FROM routes r
+        JOIN nodes n1 on r.start = n1.id
+        JOIN nodes n2 on r.end = n2.id
+        WHERE r.path LIKE :node";
+        $stmt = $this->conn->prepare($query);
+        $node = htmlspecialchars(strip_tags($node));
+        $node = "%" . $node . "%";
+        $stmt->bindParam(":node", $node);
+        $stmt->execute();
+        $result = $stmt;
+        $num_of_related_path = $result->rowCount();
+        $route_numbers = array();
+        $related_paths = array();
+        $all_related_nodes = array();
+        $cheet_sheet = array();
 
-    public function read_routeToNode($node){}
+        if ($num_of_related_path) {
+            $info_array['complete_paths'] = array();
+            $info_array['divided_paths'] = array();
+            $i = 0;
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                extract($row);
+                $info_array['complete_paths'][$i] = array();
+                $info_array['divided_paths'][$i] = array();
+                array_push($info_array['complete_paths'][$i], $path);
+                array_push($related_paths, $path);
+                array_push($route_numbers, (string)$route_no);
+                $info_array['complete_paths'][$i] = $this->path_string_to_array($info_array['complete_paths'][$i]);
+                $i++;
+            }
+
+            // basically explodes paths, 
+            // then unionizes exploded nodes and then returns them
+            $all_related_nodes = $this->path_string_to_array($related_paths);
+            //uses returned node mentioned above to make a select query
+            $fullquery = $this->generate_query($all_related_nodes);
+            $stmt2 = $this->conn->prepare($fullquery);
+            $stmt2->execute();
+            $result2 = $stmt2;
+            $num_of_related_nodes = $result2->rowCount();
+            for ($i = 0; $i < $num_of_related_nodes; $i++) {
+                $row2 = $result2->fetch(PDO::FETCH_ASSOC);
+                array_push($cheet_sheet, $row2);
+            }
+            $i = 0;
+            for ($i = 0; $i < sizeof($info_array['complete_paths']); $i++) {
+                for ($j = 0; $j < sizeof($info_array['complete_paths'][$i]); $j++) {
+                    $one_place = $info_array['complete_paths'][$i][$j];
+                    $index = array_search($one_place, array_column($cheet_sheet, 'name'));
+                    $info_array['complete_paths'][$i][$j] = $cheet_sheet[$index];
+                }
+            }
+
+            //making array unique
+            $array1_keys = array_keys($route_numbers);
+            $unique = array_keys(array_unique($route_numbers));
+            $duplicate = array_diff($array1_keys, $unique);
+            foreach ($duplicate as $index) {
+                $route_numbers[$index] =  $route_numbers[$index] . "repeated" . $index;
+            }
+            $response = array();
+            //making key response a key value pair, where key is route_no.
+            for ($i = 0; $i < sizeof($info_array['complete_paths']); $i++) {
+                $response[$route_numbers[$i]] = $info_array['complete_paths'][$i];
+            }
+            echo json_encode(
+                array(
+                    "code" => 200,
+                    "message" => $response
+                )
+            );
+        }
+    }
